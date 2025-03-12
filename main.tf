6fae55f23c5e2dbf02b22d620a00ff0bed5dabf3
sreenathprem <sreenath.premnadh@hashicorp.com>
2025-03-12 16:51:10 +0100

    Added info for gha

---
+++ main.tf
@@ -0,0 +1,53 @@
+terraform {
+  backend "gcs" {
+    bucket  = var.bucket_name
+    prefix  = "terraform/state"
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "my-vm-instance"
+  machine_type = "e2-medium"
+  zone         = "${var.region}-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      # Assigns an external IP
+    }
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+  tags = ["web", "dev"]
+}
+
+variable "project_id" {
+  description = "Google Cloud project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "Google Cloud region"
+  type        = string
+  default     = "us-central1"
+}
+
+output "instance_ip" {
+  description = "Public IP of the VM"
+  value       = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+}
